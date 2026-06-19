f6bfd046065dfd04899b1d8ffad70f20b1aaeeed
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2026-06-19 17:08:21 -0600

    T134/ Corregir import registerRequest en RegisterForm.jsx

--- src/features/auth/components/RegisterForm.jsx
+++ src/features/auth/components/RegisterForm.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { registerRequest } from '../../../shared/api/index.js';
+import { register as registerRequest } from '../../../shared/api/index.js';
 import { showSuccess, showError } from '../../../shared/utils/toast.js';
 import { Spinner } from '../../../shared/ui/Spinner.jsx';
 
